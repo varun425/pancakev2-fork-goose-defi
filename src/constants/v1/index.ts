70da1712caa0ec5eff9b5c0cbc84bc45fb66bc7f
varun.arya <arun@antiersolutions.com>
2024-08-09 17:43:49 +0530

    goose-amm

---
+++ src/constants/v1/index.ts
@@ -0,0 +1,14 @@
+import { Interface } from '@ethersproject/abi'
+import { ChainId } from '@pancakeswap-libs/sdk'
+import V1_EXCHANGE_ABI from './v1_exchange.json'
+import V1_FACTORY_ABI from './v1_factory.json'
+
+const V1_FACTORY_ADDRESSES: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0xC07d4604400139108BbdB3076636365A385879eF', // TODO
+  [ChainId.BSCTESTNET]: '0x5c80C0c5947Dec3Cbd91F0d6a323Bf14D630e734'
+}
+
+const V1_FACTORY_INTERFACE = new Interface(V1_FACTORY_ABI)
+const V1_EXCHANGE_INTERFACE = new Interface(V1_EXCHANGE_ABI)
+
+export { V1_FACTORY_ADDRESSES, V1_FACTORY_INTERFACE, V1_FACTORY_ABI, V1_EXCHANGE_INTERFACE, V1_EXCHANGE_ABI }
